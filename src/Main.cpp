1932dbff855d7041e8389da8bb6f82c8371456c4
Benedikt Maria Beckermann <hello@benedikt.click>
2025-02-24 15:27:47 +0100

    * Changed Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,22 +1,39 @@
+#include <InsertionSort.h>
 #include <iostream>
 #include <ostream>
-#include <bitset>
-#include <InsertionSortOVC.h>
 
-#include <OVC.h>
 #include <Quicksort.h>
 
 int main(int argc, char *argv[]) {
 
   Quicksort quicksort;
 
-  Record records[] = genR
-  Stats stats = insertionSortOvc.sort(records, 4, 3);
 
-  for (int i = 0; i < 4; i++) {
-    std::cout << records[i].key << " " << std::bitset<32>(records[i].ovc) << std::endl;
+  Record * records = generateRecords();
+  Stats stats = quicksort.sort(records, 99, 3, 1);
+
+
+  std::cout << "Quicksort: " << std::endl;
+  std::cout << "row: " << stats.rowComparisons << ", col: " << stats.columnComparisons<< std::endl;
+
+  for (int i = 0; i < 99; i++) {
+    std::cout << records[i].key << ", ";
   }
+  std::cout << std::endl;
 
+
+
+  std::cout << "Insertion sort: " << std::endl;
+  InsertionSort insertionsort;
+
+  records = generateRecords();
+  stats = insertionsort.sort(records, 99, 3);
+  std::cout << "row: " << stats.rowComparisons << ", col: " << stats.columnComparisons<< std::endl;
+
+  for (int i = 0; i < 99; i++) {
+    std::cout << records[i].key << ", ";
+  }
+  std::cout << std::endl;
 
   return 0;
 }
