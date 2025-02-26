f2cffcd826e57026049c36e0ccb6bd0a8f757dea
Benedikt Maria Beckermann <hello@benedikt.click>
2025-02-26 19:51:59 +0100

    * Removed recusiveness of Q_AOVC

--- src/sort/QuicksortAOVC.cpp
+++ src/sort/QuicksortAOVC.cpp
@@ -1,83 +1,94 @@
 #include "QuicksortAOVC.h"
 
 #include <iostream>
+#include <stack>
 
 #include "AOVC.h"
 
+struct Limits {
+  int left, right;
+};
 
 Stats QuicksortAOVC::sort(Record* records, int length, int keyLength, int M) {
   stats = getNewStats();
-  sort(records, 0, length, keyLength, M);
-  return stats;
-}
+  std::stack<Limits> stack;
+  int left, right;
 
-void QuicksortAOVC::sort(Record* records, int left, int right, int keyLength, int M) {
+  auto *lowest = new Record[length];    int lowestCounter = 0;
+  auto *lower = new Record[length];     int lowerCounter = 0;
+  auto *equal = new Record[length];     int equalCounter = 0;
+  auto *greater = new Record[length];   int greaterCounter = 0;
+  auto *greatest = new Record[length];  int greatestCounter = 0;
 
-  if (right - left <= 1)
-    return ;
+  stack.push(Limits(0, length));
 
-  if (right - left < M) {
-    std::cerr << "Implement Switch to Insertion Sort" << std::endl;
-    return;
-  }
+  while (!stack.empty()) {
+    auto [t_left, t_right] = stack.top();
+    stack.pop();
+    left = t_left;
+    right = t_right;
 
-  auto *lowest = new Record[right - left];    int lowestCounter = 0;
-  auto *lower = new Record[right - left];     int lowerCounter = 0;
-  auto *equal = new Record[right - left];     int equalCounter = 0;
-  auto *greater = new Record[right - left];   int greaterCounter = 0;
-  auto *greatest = new Record[right - left];  int greatestCounter = 0;
+    lowestCounter = 0;
+    lowerCounter = 0;
+    equalCounter = 0;
+    greaterCounter = 0;
+    greatestCounter = 0;
 
-  const Record pivot = records[left];
-  equal[equalCounter++] = records[left];
+    const Record pivot = records[left];
+    equal[equalCounter++] = records[left];
 
-  for (int i = left + 1; i < right; i++) {
-    stats.rowComparisons++;
-    if (records[i].aovc < pivot.aovc) {
-      lowest[lowestCounter++] = records[i];
-    } else if (records[i].aovc > pivot.aovc ) {
-      greatest[greatestCounter++] = records[i];
-    } else {
-      int offset = offsetAOVC(records[i].aovc, keyLength) + 1;
-      while (offset < keyLength) {
-        stats.columnComparisons++;
-        if (records[i].key[offset] != pivot.key[offset])
-          break;
-        offset++;
-      }
-      if (offset >= keyLength) {
-        equal[equalCounter++] = records[i];
-      } else if (records[i].key[offset] < pivot.key[offset]) {
-        records[i].aovc = genAOVC(keyLength, true, offset, records[i].key[offset]);
-        lower[lowerCounter++] = records[i];
+    for (int i = left + 1; i < right; i++) {
+      stats.rowComparisons++;
+      if (records[i].aovc < pivot.aovc) {
+        lowest[lowestCounter++] = records[i];
+      } else if (records[i].aovc > pivot.aovc ) {
+        greatest[greatestCounter++] = records[i];
       } else {
-        records[i].aovc = genAOVC(keyLength, false, offset, records[i].key[offset]);
-        greater[greaterCounter++] = records[i];
-      }
+        int offset = offsetAOVC(records[i].aovc, keyLength) + 1;
+        while (offset < keyLength) {
+          stats.columnComparisons++;
+          if (records[i].key[offset] != pivot.key[offset])
+            break;
+          offset++;
+        }
+        if (offset >= keyLength) {
+          equal[equalCounter++] = records[i];
+        } else if (records[i].key[offset] < pivot.key[offset]) {
+          records[i].aovc = genAOVC(keyLength, true, offset, records[i].key[offset]);
+          lower[lowerCounter++] = records[i];
+        } else {
+          records[i].aovc = genAOVC(keyLength, false, offset, records[i].key[offset]);
+          greater[greaterCounter++] = records[i];
+        }
 
+      }
     }
+
+    int counter = left;
+
+    move(records, lowest, counter, lowestCounter);
+    move(records, lower, counter, lowerCounter);
+    move(records, equal, counter, equalCounter);
+    move(records, greater, counter, greaterCounter);
+    move(records, greatest, counter, greatestCounter);
+
+    int l = left;
+    if (lowestCounter > M) stack.push(Limits(l, l+lowestCounter));
+    l+=lowestCounter;
+    if (lowerCounter > M) stack.push(Limits(l, l+lowerCounter));
+    l+=lowerCounter+equalCounter;
+    if (greaterCounter > M) stack.push(Limits(l, l+greaterCounter));
+    l+=greaterCounter;
+    if (greatestCounter > M) stack.push(Limits(l, l+greatestCounter));
+
   }
 
-  int counter = left;
 
-  move(records, lowest, counter, lowestCounter);
-  move(records, lower, counter, lowerCounter);
-  move(records, equal, counter, equalCounter);
-  move(records, greater, counter, greaterCounter);
-  move(records, greatest, counter, greatestCounter);
-
-  int l = left;
-  sort(records, l, l+lowestCounter, keyLength, M);
-  l+=lowestCounter;
-  sort(records, l, l+lowerCounter, keyLength, M);
-  l+=lowerCounter+equalCounter;
-  sort(records, l, l+greaterCounter, keyLength, M);
-  l+=greaterCounter;
-  sort(records, l, l+greatestCounter, keyLength, M);
+  return stats;
 }
 
 void QuicksortAOVC::move(Record* records, const Record * list, int &counter, const int length) {
   for (int i = 0; i < length; i++) {
     records[counter++] = list[i];
   }
-  delete[] list;
 }
--- src/sort/QuicksortAOVC.h
+++ src/sort/QuicksortAOVC.h
@@ -10,6 +10,5 @@ class QuicksortAOVC {
     Stats sort(Record* records, int length, int keyLength, int M);
 
   private:
-    void sort(Record* records, int left, int right, int keyLength, int M);
     static void move(Record *records, const Record *list, int &counter, int length);
 };
