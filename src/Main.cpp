a183f355f306e435eae3c0adc76c97a3ce4d7341
Benedikt Maria Beckermann <hello@benedikt.click>
2025-03-25 11:44:04 +0100

    Slightly adapted Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,19 +13,24 @@ int main(int argc, char *argv[]) {
     MergesortOVC mergesort;
 
 
-    int N = 100000;
+    int N = 5000000;
     int k = 3;
-    Record * records = generateRecords(N, k, 1338);
+    int seed = 8;
+
+    Record * records = generateRecords(N, k, seed);
 
     auto [rowComparisonsM, columnComparisonsM] = mergesort.sort(records, N, k);
 
     std::cout << "MergesortOVC: RowCmp: " << rowComparisonsM << ", ColCmp: " << columnComparisonsM << std::endl;
 
-    records = generateRecords(N, k, 1338);
+    records = generateRecords(N, k, seed);
 
     QuicksortAOVC quicksort_aovc;
-    auto [rowComparisonsQ, columnComparisonsQ] = quicksort_aovc.sort(records, N, k, 1);
+    Stats stats = quicksort_aovc.sort(records, N, k, 1);
+    std::cout << "QuicksortOVC: RowCmp: " << stats.rowComparisons << ", ColCmp: " << stats.columnComparisons  << std::endl;
 
-    std::cout << "QuicksortOVC: RowCmp: " << rowComparisonsQ << ", ColCmp: " << columnComparisonsQ  << std::endl;
+    QuicksortAOVC::updateToPositiveAOVC(records, N, k, stats);
+
+    std::cout << "QuicksortOVC (fixed AOVC): RowCmp: " << stats.rowComparisons << ", ColCmp: " << stats.columnComparisons  << std::endl;
 }
 
