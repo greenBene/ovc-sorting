014910c7683af7ea860bfadab264c29f8de88818
Benedikt Maria Beckermann <hello@benedikt.click>
2025-03-25 17:14:42 +0100

    Implemented basic CLI

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,36 +1,112 @@
-#include <HeapsortOVC.h>
 #include <iostream>
-#include <Mergesort.h>
-#include <MergesortOVC.h>
 #include <ostream>
-#include <Quicksort.h>
+#include <fstream>
+#include <string>
+
+#include <boost/program_options.hpp>
+
 #include <QuicksortAOVC.h>
 #include <QuicksortOVC.h>
 
 
+namespace po = boost::program_options;
+
+
+
+struct Result {
+    Record * records;
+    int N;
+    int k;
+};
+
+Result readKeyFile(const std::string &path) {
+    if (std::ifstream file(path); file.is_open()) {
+        std::vector<std::string> keys;
+        std::string line;
+
+        while (getline(file, line)) {
+            keys.push_back(line);
+        }
+
+        const int k = keys[0].length();
+
+        for (int i = 1; i < keys.size(); i++) {
+            if (keys[i].length() != k) {
+                std::cerr << "Invalid input. Not all keys are of same size ";
+                return {nullptr, 0, 0};
+            }
+        }
+
+        const int N = keys.size();
+
+        return {generateRecords(keys, keys.size()), N, k};
+    }
+    std::cerr << "Could not read file successfully." << std::endl;
+    return {nullptr, 0, 0};
+}
+
+
 int main(int argc, char *argv[]) {
+    try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "Produce help message")
+            ("algorithm", po::value<std::vector<std::string>>()->multitoken()->required(),
+                "Algorithm used to sort data. \n"
+                "Available: heapsort, heapsortovc, insertionsort, "
+                "insertionsortovc, mergesort, mergesortovc, "
+                "quicksort, quicksortOVC, quicksortAOVC")
+            ("input,I", po::value<std::string>()->required(), "File to sort")
+        ;
 
-    MergesortOVC mergesort;
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        notify(vm);
 
+        if (vm.contains("help")) {
+            std::cout << desc << std::endl;
+            return 0;
+        }
 
-    int N = 5000000;
-    int k = 3;
-    int seed = 8;
+        auto [records, N, k] = readKeyFile(vm["input"].as<std::string>());
+        if (records == nullptr) {
+            std::cerr << "Invalid Input File";
+            return -1;
+        }
 
-    Record * records = generateRecords(N, k, seed);
+        std::cout << "Input -- N: " << N << ", k: " << k << std::endl;
 
-    auto [rowComparisonsM, columnComparisonsM] = mergesort.sort(records, N, k);
+        for (int i = 0; i < N; i++) {
+            std::cout << records[i].key << " ";
+        }
+        std::cout << std::endl;
 
-    std::cout << "MergesortOVC: RowCmp: " << rowComparisonsM << ", ColCmp: " << columnComparisonsM << std::endl;
+        if (vm.contains("algorithm")) {
+            auto algorithms = vm["algorithm"].as<std::vector<std::string>>();
 
-    records = generateRecords(N, k, seed);
+            for (auto algorithm : algorithms) {
+                if (algorithm == "quicksort") {
 
-    QuicksortAOVC quicksort_aovc;
-    Stats stats = quicksort_aovc.sort(records, N, k, 1);
-    std::cout << "QuicksortOVC: RowCmp: " << stats.rowComparisons << ", ColCmp: " << stats.columnComparisons  << std::endl;
+                    std::cout << "QUICKSORT" << std::endl;
+                } else if (algorithm == "quicksortovc") {
+                    QuicksortOVC quicksortOvc;
+                    quicksortOvc.sort(records, N, k, 1);
+                } else {
+                    std::cout << "Algorithm not implemented: " << algorithm << std::endl;
+                }
+            }
+        }
 
-    QuicksortAOVC::updateToPositiveAOVC(records, N, k, stats);
+        for (int i = 0; i < N; i++) {
+            std::cout << records[i].key << " ";
+        }
+        std::cout << std::endl;
 
-    std::cout << "QuicksortOVC (fixed AOVC): RowCmp: " << stats.rowComparisons << ", ColCmp: " << stats.columnComparisons  << std::endl;
+        std::cout << "Hello World" << std::endl;
+    } catch (std::exception& e ) {
+        std::cerr << "error:" << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Exception of unknown type" << std::endl;
+    }
 }
 
--- src/sort/Record.h
+++ src/sort/Record.h
@@ -27,6 +27,17 @@ static Record* generateRecords(std::string* keys, int length, uint32_t ovc = OVC
     return records;
 }
 
+static Record* generateRecords(std::vector<std::string> keys, int length, uint32_t ovc = OVC_MINUS_INFINITY, uint32_t aovc = AOVC_MINUS_INFINITY, uint32_t ovcSibling = OVC_MINUS_INFINITY) {
+    Record* records = new Record[length];
+    for (int i = 0; i < length; i++) {
+        records[i].key = keys[i];
+        records[i].ovc = ovc;
+        records[i].aovc = aovc;
+        records[i].siblingOVC = ovcSibling;
+    }
+    return records;
+}
+
 static Record* generateRecords(unsigned int n, unsigned int keyLength, unsigned int seed, uint32_t ovc = OVC_PLUS_INFINITY, uint32_t aovc = AOVC_MINUS_INFINITY) {
     srand(seed);
     auto * records = new Record[n];
